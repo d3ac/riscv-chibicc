7f7c1ac2819d2bd80df926611044b9b4502c0dc2
d3ac <1048563083@qq.com>
2024-06-09 12:42:23 +0800

    le2

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
+PARAMS_FILE := input.in
+PARAMS := $(shell cat $(PARAMS_FILE))
+
 rvcc:
 	riscv64-unknown-elf-gcc -o rvcc main.c
 
 test: rvcc # 测试rvcc
-	qemu-riscv64 -L $RISCV/sysroot rvcc 33
-	qemu-riscv64 -L $RISCV/sysroot rvcc 33 > tmp.s
+	qemu-riscv64 -L $RISCV/sysroot rvcc $(PARAMS)
+	qemu-riscv64 -L $RISCV/sysroot rvcc $(PARAMS) > tmp.s
 	chmod +x *.sh
 	./test.sh
 
-clean: # 清理编译产生的文件
+clear: # 清理编译产生的文件
 	rm -f rvcc *.o *.s tmp* a.out
--- main.c
+++ main.c
@@ -12,25 +12,24 @@ int main(int Argc, char *Argv[]){
     printf("  .globl main\n");
     printf("main:\n");
 
-    printf("  li a0, %d\n", atoi(Argv[1]));
-    printf("  ret\n");
-    // // 获取传入的参数, strol函数将字符串转换为长整型, 第一个参数为字符串, 第二个参数为指向字符串的指针, 第三个参数为进制
-    // // 运行完了之后, P指向字符串的下一个字符 (非数字字符)
-    // char *P = Argv[1];
-    // printf("  li a0, %ld\n", strtol(P, &P, 10));
+    // 获取传入的参数, strol函数将字符串转换为长整型, 第一个参数为字符串, 第二个参数为指向字符串的指针, 第三个参数为进制
+    // 运行完了之后, P指向字符串的下一个字符 (非数字字符)
+    char *P = Argv[1];
+    printf("  li a0, %ld\n", strtol(P, &P, 10));
     
-    // while(*P){
-    //     if(*P == '+'){
-    //         ++P; // 跳过+
-    //         printf("  addi a0, a0, %ld\n", strtol(P, &P, 10));
-    //         continue;
-    //     }
-    //     else if(*P == '-'){
-    //         ++P; // 跳过-
-    //         printf("  subi a0, a0, -%ld\n", strtol(P, &P, 10)); // riscv没有subi指令, 所以用subi a0, a0, -x代替
-    //         continue;
-    //     }
-        
-    // }
+    while(*P){
+        if(*P == '+'){
+            ++P; // 跳过+
+            printf("  addi a0, a0, %ld\n", strtol(P, &P, 10));
+            continue;
+        }
+        else if(*P == '-'){
+            ++P; // 跳过-
+            printf("  subi a0, a0, -%ld\n", strtol(P, &P, 10)); // riscv没有subi指令, 所以用subi a0, a0, -x代替
+            continue;
+        }
+    }
+
+    printf("  ret\n");
     return 0;
 }
